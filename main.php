87295c585962faa83bcc67230da34cefc285a43a
JARVIS\Harry <amazingharry95@gmail.com>
2017-04-30 16:27:01 +0700

    coba modal

--- main.php
+++ main.php
@@ -164,9 +164,9 @@
                                   <td>Gian Sebastian Anjasmara </td>
                                   <td><span class="label label-success label-mini">Healthy</span></td>
                                   <td>
-                                      <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
-                                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
-                                      <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+                                      <button class="btn btn-success btn-xs"><i class="glyphicon glyphicon-eye-open"></i></button>
+                                      <button class="btn btn-primary btn-xs"><i class="	glyphicon glyphicon-edit"></i></button>
+                                      <a href="#" class="confirm-delete" data-id="1"><button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button></a>
                                   </td>
                               </tr>
                               </tbody>
@@ -176,6 +176,21 @@
               </div>
           </section>
       </section>
+      
+      <div id="myModal" class="modal hide">
+        <div class="modal-header">
+            <a href="#" data-dismiss="modal" aria-hidden="true" class="close">x</a>
+            <h3>Delete</h3>
+        </div>
+        <div class="modal-body">
+            <p>You are about to delete.</p>
+            <p>Do you want to procedd?</p>
+        </div>
+        <div class="modal-footer">
+            <a href="#" id="btnYes" class="btn danger">Yes</a>
+            <a href="#" data-dismiss="modal" aria-hidden="true" class="btn secondary">No</a>
+        </div>
+      </div>
 
       <!--main content end-->
       <!--footer start-->
@@ -288,6 +303,27 @@
               }
             }
     </script>
+    
+      <script>
+        $('#myModal').on('show', function() {
+    var id = $(this).data('id'),
+        removeBtn = $(this).find('.danger');
+})
+
+$('.confirm-delete').on('click', function(e) {
+    e.preventDefault();
+
+    var id = $(this).data('id');
+    $('#myModal').data('id', id).modal('show');
+});
+
+$('#btnYes').click(function() {
+    // handle deletion here
+  	var id = $('#myModal').data('id');
+  	$('[data-id='+id+']').remove();
+  	$('#myModal').modal('hide');
+});
+      </script>
   
 
   </body>
